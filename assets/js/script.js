270142446463a84c605e0ec106137c8b1ab9f8a6
astro <angelica.strong0725@gmail.com>
2024-01-13 21:37:27 -0800

    altered generate password function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,24 +3,26 @@ var generateBtn = document.querySelector("#generate");
 
 // function to give criteria for password
 function generatePassword() {
-var length = parseInt(prompt("Enter password length (8-128 characters):"));
-
-// validating length
-if (isNaN(length) || length < 8 || length > 128) {
-  alert("Please enter a valid password length between 8 and 128 characters.");
-  return ""
-}
+  var passwordLength = prompt("Enter the length of the password (8-128 characters):");
+  // Validating password length
+  if(passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+      alert("Password length must be between 8 and 128 characters.");
+      return "";
+  }
 
 var lowercase = confirm("Include lowercase characters?")
 var uppercase = confirm("Include uppercase characters?")
 var numeric = confirm("Include numeric characters?")
 var specialChars = confirm("Include special characters?")
 
+
 // added valid characters to generate password
 var lowercaseChars = "abcdefghijklmnopqrstuvwxyz"
 var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var numericChars = "0123456789"
 var specialChars = "!@#$%^&*()_+[]{}|;:,.<>?"
+
+}
 
 return generatedPassword
 }
